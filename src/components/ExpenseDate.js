355c08a26762371bbac1f88dbd880bebec7db4b9
matheusarb <mat.araujoribeiro@gmail.com>
2022-10-04 21:57:03 -0300

    components props interchangeability

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+//components in React are functions that returns JSX code
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("eng-us", { month: "long" });
+  const day = props.date.toLocaleString("eng-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  //it is best practice to separate large chunks of JS logic from your JSX code, as shown below
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
