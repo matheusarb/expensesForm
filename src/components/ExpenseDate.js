9d66d7408877b37b947ce1999ea6df787090346e
matheusarb <mat.araujoribeiro@gmail.com>
2022-10-04 22:17:43 -0300

    ExpenseDate.css update

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,3 +1,5 @@
+import './ExpenseDate.css';
+
 //components in React are functions that returns JSX code
 
 function ExpenseDate(props) {
@@ -6,10 +8,10 @@ function ExpenseDate(props) {
   const year = props.date.getFullYear();
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
