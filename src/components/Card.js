84ed408648891283ce020cde0116d7b680771148
matheusarb <mat.araujoribeiro@gmail.com>
2022-10-07 23:49:45 -0300

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,2 +1,9 @@
 //Card.js
-//Tipically used to describe Elements that contains
+//Tipically used to describe containers with round looks, drop-shadows and other elements
+import './Card.css';
+
+function Card(props) {
+    return <div className="card">{props.children}</div>
+}
+
+export default Card;
