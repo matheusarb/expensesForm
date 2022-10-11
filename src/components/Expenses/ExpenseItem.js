f35902cacf598673aa97485a2b547e672f1d2795
matheusarb <mat.araujoribeiro@gmail.com>
2022-10-10 23:28:18 -0300

    adding event listeners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,20 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   //it is best practice to separate large chunks of JS logic from your JSX code, as shown below
+  let title = props.title;
+  
+  const clickHandler = () => {
+    console.log('Clicked!');
+  }
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
