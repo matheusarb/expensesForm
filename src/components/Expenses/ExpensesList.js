82aee7a19e35cd9c0146eaf2a63c0e5ae1bbdaaf
matheusarb <mat.araujoribeiro@gmail.com>
2022-10-19 15:57:16 -0300

    update previous commit

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import './ExpensesList.css';
+import React from "react";
 
-const  ExpensesList = props => {
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
 
-}
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">No expenses found.</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          //key renderiza o id do elemento para manter a adição organizada
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default ExpensesList;
