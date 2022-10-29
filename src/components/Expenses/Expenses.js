6cbaa3f396e6f591ec18e771e1f42240006a461b
matheusarb <mat.araujoribeiro@gmail.com>
2022-10-28 23:07:23 -0300

    update final snippet of the project

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
-import ExpensesChart from './ExpensesChart';
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
